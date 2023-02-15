1f0ea6bb5678a309817621301120fa06477270f7
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-02-15 17:13:42 -0300

    Import Ramda

--- script.js
+++ script.js
@@ -7,9 +7,33 @@ class Book {
     this.isRead = false;
   }
 
-  getBookInfo = function () {
+  bookInfo = function () {
     return `Title: ${this.title}, Author: ${this.author}, Number of pages: ${this.numberOfPages}, Is Read: ${this.isRead}`;
   };
 }
+
+/* dummy book objs */
+const book1 = new Book("War of the worlds", "H. G. Wells", 350);
+const book2 = new Book("My little pony saga", "Scholastic", 100000000);
+let bookArray = [book1, book2];
+
+/* dom stuff */
+const newEl = (tag) => document.createElement(tag);
+
+const addText = (element, text) => (element.textContent = text);
+
+const appendNode = (library, book) => library.appendChild(book);
+
+const addClassName = (element, className) => element.classList.add(className);
+
+const bookData = (book) => book.bookInfo();
+
+
+const myLibrary = document.querySelector("#library");
+
+/* create div element with book info for length of array */
+bookArray.forEach((book) => {
+
+});
 
 
