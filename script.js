575c7c003f1f751d9dd9ca7ac08914ea3c3e6cc3
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-02-16 22:49:52 -0300

    Trying classes again

--- script.js
+++ script.js
@@ -1,39 +1,26 @@
 class Book {
-  constructor(title, author, numberOfPages) {
-    this.title = title;
-    this.author = author;
-    this.numberOfPages = numberOfPages;
-
-    this.isRead = false;
+  constructor() {
+    this.title = document.querySelector("#title").value;
+    this.author = document.querySelector("#author").value;
+    this.hasRead = false;
   }
 
-  bookInfo = function () {
-    return `Title: ${this.title}, Author: ${this.author}, Number of pages: ${this.numberOfPages}, Is Read: ${this.isRead}`;
-  };
+  get title() {
+    return this.title;
+  }
+
+  get author() {
+    return this.author;
+  }
+
+  set hasRead(read) {
+    const radio = document.querySelector("#radio-btn");
+    this.hasRead = read;
+  }
 }
 
-/* dummy book objs */
-const book1 = new Book("War of the worlds", "H. G. Wells", 350);
-const book2 = new Book("My little pony saga", "Scholastic", 100000000);
-let bookArray = [book1, book2];
-
-/* dom stuff */
-const newEl = (tag) => document.createElement(tag);
-
-const addText = (element, text) => (element.textContent = text);
-
-const appendNode = (library, book) => library.appendChild(book);
-
-const addClassName = (element, className) => element.classList.add(className);
-
-const bookData = (book) => book.bookInfo();
-
-
-const myLibrary = document.querySelector("#library");
-
-/* create div element with book info for length of array */
-bookArray.forEach((book) => {
-
-});
-
-
+class Library {
+  constructor() {
+    this.books = [];
+  }
+}
