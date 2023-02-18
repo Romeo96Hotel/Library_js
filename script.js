8b1b849202ea6db35f8a1b024049ac015e871368
romeo hotel <90534544+Romeo96Hotel@users.noreply.github.com>
2023-02-18 18:34:56 -0300

    Class approach

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
+/* 
+
+button is clicked
+    get title value
+    get author value
+    get number of pages 
+    get is read  
+    
+    create container for book
+    create element for title
+    create element for author
+    create element for number of pages
+    create element for is read
+    create remove button
+
+    append element to container
+
+    append container to library
+    
+
+*/
+
+
 class Book {
-  constructor() {
-    this.title = document.querySelector("#title").value;
-    this.author = document.querySelector("#author").value;
-    this.hasRead = false;
+    constructor() {
+      this.title = document.querySelector("#title");
+      this.author = document.querySelector("#author");
+      this.numberOfPages = ;
+  
+      this.isRead = false;
+    }
+  
+    getBookInfo = function () {
+      return `Title: ${this.title}, Author: ${this.author}, Number of pages: ${this.numberOfPages}, Is Read: ${this.isRead}`;
+    };
   }
-
-  get title() {
-    return this.title;
-  }
-
-  get author() {
-    return this.author;
-  }
-
-  set hasRead(read) {
-    const radio = document.querySelector("#radio-btn");
-    this.hasRead = read;
-  }
-}
-
-class Library {
-  constructor() {
-    this.books = [];
-  }
-}
+  
